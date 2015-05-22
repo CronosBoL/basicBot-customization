a0675e8664b88cf62b560cc5955557d360ece43a
CronosBoL <giovanni.yaeger@yahoo.com>
2015-05-22 15:57:56 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -195,7 +195,7 @@
             language: "english",
             chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
             startupCap: 1, // 1-200
-            startupVolume: 0, // 0-100
+            startupVolume: 15, // 0-100
             startupEmoji: false, // true or false
             cmdDeletion: true,
             maximumAfk: 120,
